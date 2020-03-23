f04f00769e5c633798a57ff248ee3be2db14ea0c
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2020-03-23 01:25:53 +0000

    [WIP] Parsing the string to JSON

--- main.go
+++ main.go
@@ -5,10 +5,18 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 )
+
+type Field struct {
+	FieldType          string
+	FieldName          string
+	FieldFlags         float64
+	FieldJustification string
+}
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
@@ -19,7 +27,6 @@ func main() {
 		file, err := c.FormFile("file")
 
 		if err != nil {
-			println("No file")
 			c.Error(err)
 			return
 		}
@@ -43,11 +50,59 @@ func main() {
 
 		output := string(out[:])
 
-		fields := strings.Split(output, "")
+		pdftkFields := strings.Split(output, "---\n")
 
-		res := []string{"foo", "bar", string(output)}
+		// Remove the empty string from the start of the fields list
+		_, pdftkFields = pdftkFields[0], pdftkFields[1:]
+
+		var fields []Field
+		for _, pdftkField := range pdftkFields[1:] {
+			fmt.Println("This is the field")
+			fmt.Println(pdftkField)
+			fieldData := strings.Split(pdftkField, "\n")
+
+			// Remove the empty string at the end of the field data list
+			fieldData = fieldData[:len(fieldData)-1]
+
+			fieldDataMap := make(map[string]string)
+			for _, data := range fieldData {
+				parts := strings.Split(data, ": ")
+				fmt.Println(strings.Join(parts, "--------"))
+				fmt.Println(parts[0])
+				fmt.Println(parts[1])
+				fieldDataMap[parts[0]] = parts[1]
+			}
+
+			// fmt.Println("This is the field after split")
+			// fmt.Println(fieldData)
+			// fieldType := fieldData[0]
+			// fieldName := fieldData[1]
+			fieldFlags, err := strconv.ParseFloat(fieldDataMap["FieldFlags"], 64)
+
+			if err != nil {
+				c.Error(err)
+				return
+			}
+
+			// fieldJustification := fieldData[3]
+
+			field := Field{
+				FieldType:          fieldDataMap["FieldType"],
+				FieldName:          fieldDataMap["FieldName"],
+				FieldFlags:         fieldFlags,
+				FieldJustification: fieldDataMap["FieldJustification"],
+			}
+
+			fields = append(fields, field)
+		}
+
+		jsonFields, err := json.Marshal(fields)
+		fmt.Println(string(jsonFields))
+
+		res := []string{"foo", "bar", string(jsonFields)}
 		c.JSON(200, res)
-	})
+	},
+	)
 
 	r.POST("/fill-pdf", func(c *gin.Context) {
 		file, err := c.FormFile("file")
