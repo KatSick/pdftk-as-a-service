98e81a14467c97a5d9a21b7d91695c7cb7bad976
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2020-03-23 00:05:41 +0000

    Get the endpoint to return the dump_data_fields command

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +14,40 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 
 	r := gin.Default()
+
+	r.GET("/dump-data-fields", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+
+		if err != nil {
+			println("No file")
+			c.Error(err)
+			return
+		}
+
+		// Create the tempdir and tempfile for pdftk to operate on
+		tempdir := os.TempDir()
+		tempfile := tempdir + "/temp.pdf"
+
+		err = c.SaveUploadedFile(file, tempfile)
+
+		args := []string{
+			tempfile,
+			"dump_data_fields",
+		}
+
+		out, err := exec.Command("pdftk", args...).Output()
+
+		if err != nil {
+			println("pdftk error: " + err.Error())
+		}
+
+		output := string(out[:])
+
+		fields := strings.Split(output, "")
+
+		res := []string{"foo", "bar", string(output)}
+		c.JSON(200, res)
+	})
 
 	r.POST("/fill-pdf", func(c *gin.Context) {
 		file, err := c.FormFile("file")
@@ -44,6 +81,7 @@ func main() {
 
 		c.File(tempfilefilled)
 	})
-
-	r.Run(":80")
+	port := ":9092"
+	fmt.Print("Running on port " + port)
+	r.Run(port)
 }
